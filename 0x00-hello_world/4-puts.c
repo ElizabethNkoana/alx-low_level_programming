269316f4f3f98afea70839185da8ec3617fa8941
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-12 13:36:39 -0700

    My first C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print using the puts function
+ *
+ * Description - by using the main function the program will
+ * print "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
