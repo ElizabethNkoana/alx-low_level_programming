7055e9d7d16936cf7e5709c41bd9ebbadf3de845
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-16 06:16:41 -0700

    Last digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ else if (l == 0)
 }
 else
 {
-	printf("Last digit og %d is %d and is less than 6 and not 0\n", n, l);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 
 Return (0);
