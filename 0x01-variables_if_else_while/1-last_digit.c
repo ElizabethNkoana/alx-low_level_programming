54768e2efd92bda408d55acd16153a4d5478583e
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-16 05:21:18 -0700

    Last digit Commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,12 +18,12 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 l = n % 10;
 
-if (1 > 5)
-	printf("Last digit of %d and is greater than 5\n"2, n, 1);
-else if (1 == 0)
-	printf("Last digit of %d is %d and is 0\n", n, 1);
+if (l > 5)
+	printf("Last digit of %d and is greater than 5\n"2, n, l);
+else if (l == 0)
+	printf("Last digit of %d is %d and is 0\n", n, l);
 else
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 1);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 
 Return (0);
 }
