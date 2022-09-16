1db9a0c62eb69cb23ce5db4d6c3836c712e16feb
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-16 14:17:14 -0700

    Print Combination commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print a combination of single digit numbers separated by comma and space
+ * main - print single digit numbers 1-9 separated by comma and space
  *
  * Description: by using the main function
  * this program prints "1, 2, 3, 4, 5, 6, 7, 8, 9"
