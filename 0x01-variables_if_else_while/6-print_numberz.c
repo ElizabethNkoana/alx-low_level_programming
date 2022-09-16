d0119a50749dd4c52056e46ee24eed1edc84d51d
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-16 14:25:20 -0700

    Print Numberz commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 /**
- * main - print if the number is positive, zero, or negative
+ * main - print all single digit numbers of base 10 starting from 0
  *
  * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
+ * this program prints "1-9"
  * Return: 0
  */
+int main(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+	putchar(C);
+}
+putchar('\n');
+return (0);
+}
