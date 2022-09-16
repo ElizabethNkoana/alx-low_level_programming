1d755e34e397fa8899ee32b331d72d10ad30d907
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-16 06:07:24 -0700

    Last digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 /**
+ *
  * main - print if the number is positive, zero, or negative
  *
  * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
+ * this program prints "Programming is positive, zero or negative
  * Return: 0
  */
 int main(void)
 {
-
 int n;
 int l;
-	
+
 srand(time(0));
-n = rand() - RAND_MAX / 2;	
+n = rand() - RAND_MAX / 2;
 l = n % 10;
 
 if (l > 5)
@@ -28,7 +28,7 @@ else if (l == 0)
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	printf("Last digit og %d is %d and is less than 6 and not 0\n", n, l);
 }
 
 Return (0);
