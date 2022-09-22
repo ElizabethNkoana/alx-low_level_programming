2c8e37d2488f356ea491ac402e04102a46db7d77
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-22 08:57:17 -0700

    print triangle commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle -  prints a triangle using # character.
+ * @size: the size of the triangle
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+	for (i = 0; i <= size; i++)
+	{
+	for (j = size - i; j > 0; j--)
+	{
+		_putchar(' ');
+	}
+	for (j = 0; j < i; j++)
+	{
+		_putchar('#');
+	}
+	if (i == size)
+	{
+	continue;
+	}
+	_putchar('\n');
+	}
+	}
+	_putchar('\n');
+}
