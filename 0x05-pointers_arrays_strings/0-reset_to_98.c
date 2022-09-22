a1b00d94bae37bab063e5616956a3529cece3677
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-22 15:27:13 -0700

    reset to 98 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as and updates the value it points to.
- * @n: the poniter
- *
- * Return: void
+ * reset_to_98 - takes a pointer to an int as and updates the value.
+ * @n: the pointer
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
