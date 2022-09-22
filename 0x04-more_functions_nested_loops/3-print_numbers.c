fb3c84ec7f8add68e48ed9bdd3eaf5d4203a3768
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-22 05:39:35 -0700

    print numbers commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,6 +3,7 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
  *
+ * Return: 0
  */
 
 void print_numbers(void)
@@ -11,7 +12,8 @@ void print_numbers(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-		_putchar((n % 10) + '0')
+	_putchar((n % 10) + '0');
 	}
-		_putchar('\n');
+	_putchar('\n');
+	return (0);
 }
