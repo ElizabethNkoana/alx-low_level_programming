a16eb681fa571b1ba0bc18ccd0788d11d8808eb6
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-22 16:51:18 -0700

    print array commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,14 +14,14 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
-	}
+	printf("%d", a[i]);
 	if (i == n - 1)
-	{
+
 	continue;
+
+	printf(", ");
 	}
-	{
-		printf(", ");
-	}
+
 	printf("\n");
+	return (0);
 }
