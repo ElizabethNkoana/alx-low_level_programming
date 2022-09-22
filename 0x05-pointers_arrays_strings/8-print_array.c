ab2134c8402dd4289034e8aa16646e08e4967ac7
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-22 16:55:55 -0700

    print array commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,23 +5,21 @@
  * print_array - prints n elements of an array of integers
  * @a: array
  * @n: element to print
- *
- * Return: 0
  */
+
 void print_array(int *a, int n)
 {
-	int i;
+	int index;
 
-	for (i = 0; i < n; i++)
+	for (index = 0; index < n; index++)
 	{
-	printf("%d", a[i]);
-	if (i == n - 1)
+		printf("%d", a[index]);
 
-	continue;
+		if (index == n - 1)
+			continue;
 
-	printf(", ");
+		printf(", ");
 	}
 
 	printf("\n");
-	return (0);
 }
