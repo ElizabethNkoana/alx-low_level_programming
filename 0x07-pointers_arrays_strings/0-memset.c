53ed1cea8260820c4e7d89c7cd95bff5deb6e63f
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-28 16:40:51 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,13 +6,16 @@
  * @b: char to be used
  * @n: number of bytes to be used
  *
- * Return: pointer to the memory block
+ * Return: Always 0
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n)
+	int i = 0;
+
+	for (; n > 0; i++)
 	{
-		s [n - 1] = b;
+		s[i] = b;
 		n--;
 	}
 	return (s);
