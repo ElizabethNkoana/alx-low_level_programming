213e08ccb5db6e7fade84e1542dad117ec3e123b
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-09-29 15:25:39 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,15 +3,16 @@
 /**
  * _puts_recursion - Prints a string, followed by a new line.
  * @s: The string to be printed.
+ *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (s *)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return (0);
 	}
-	else
-		_putchar('\n')
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
-
