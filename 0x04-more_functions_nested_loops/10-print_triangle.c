aa836193a76861fcb70fd68e58817f029b713d86
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-10-01 19:56:07 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,29 +5,26 @@
  * @size: the size of the triangle
  *
  */
-
 void print_triangle(int size)
 {
 	int i, j;
 
 	if (size > 0)
 	{
-	for (i = 0; i <= size; i++)
-	{
-	for (j = size - i; j > 0; j--)
-	{
-		_putchar(' ');
-	}
-	for (j = 0; j < i; j++)
-	{
-		_putchar('#');
-	}
-	if (i == size)
-	{
-	continue;
-	}
-	_putchar('\n');
-	}
+		for (i = 0; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			if (i == size)
+				continue;
+			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
