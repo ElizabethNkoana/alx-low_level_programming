6c2aa4754bb45fcdcdf43431575055f55bf8a595
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-10-10 08:04:29 -0700

    task 0

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills a memory block with a constant byte
+ * @s: address to memory block
+ * @b: char to be used
+ * @n: number of bytes to be used
+ *
+ * Return: Always 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,10 @@
+0x09-static_libraries
+
+Task 0
+
+The static library libmy.a containing all functions.
+
+Task 1
+
+A script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current 
+directory.
