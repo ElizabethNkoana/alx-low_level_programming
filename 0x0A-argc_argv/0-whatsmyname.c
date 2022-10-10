7d809b7f2387d25ee9a1d5a023ee69ef96086f91
Elizabeth Nkoana <elizabethnkoana@icloud.com>
2022-10-10 10:35:47 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the program.
+ * @argc: arguments count
+ * @argv: arguments
+ *
+ * Return: always 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
